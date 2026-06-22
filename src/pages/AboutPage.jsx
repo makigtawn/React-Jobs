b145150d29ceeb4ca6959f351335543bafb26c98
makigtawn <meklitgirmaw@gmail.com>
2026-06-22 09:31:32 +0300

    update

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -5,13 +5,13 @@ import aboutImg from "../assets/images/new-images/about-img.svg";
 const About = () => {
   return (
     <section className="dark:bg-slate-900/60">
-      <div className="mx-auto max-w-5xl px-6 py-12">
+      <div className="mx-auto max-w-5xl px-6 py-32">
         
-        <h2 className="text-2xl text-center py-8 text-[#21b8b2] font-bold mb-4">
+        <h2 className="lg:text-6xl lg:py-3 md:text-6xl text-2xl  text-center text-[#21b8b2] font-bold mb-4">
           ABOUT US
         </h2>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center max-w-5xl mx-auto mb-12">
+        <div className="grid py-4 grid-cols-1 md:grid-cols-2 gap-8 items-center max-w-5xl mx-auto mb-12">
           
           <div className="flex justify-center w-full">
             <img
@@ -22,7 +22,7 @@ const About = () => {
           </div>
 
           <p className="text-white dark:text-white text-center md:text-left text-base leading-relaxed">
-            Spering was founded by a group of passionate innovators who saw how
+            Strata was founded by a group of passionate innovators who saw how
             broken and time-consuming the traditional hiring process had become.
             Our journey began with a mission to eliminate the manual grind of
             sorting through endless resumes, fueled by a vision of a smarter,
@@ -34,7 +34,7 @@ const About = () => {
           
         </div>
 
-        <div className="mt-8">
+        <div className="mt-8 py-14">
           <h3 className="text-4xl py-14 text-[#21b8b2] font-bold text-center">
             Meet Our Team
           </h3>
